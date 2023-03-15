712f6c0ab32922b96f9f7f3391d18a2ae23135f5
Brian Babu <briansmkenya@gmail.com>
2023-03-15 11:36:33 +0300

    Brian complete group assignments

---
+++ brian.js
@@ -0,0 +1,7 @@
+// Number 3 Assignment.
+function largerNumber(...nums) {
+    return Math.max(...nums);
+  }
+   console.log(largerNumber(3, 6));
+   console.log(largerNumber(2, 5, 8, 1));
+   console.log(largerNumber(-4, -2)); 
